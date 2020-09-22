e4156bbf319bfbc6e71c228b1d945c67ca67ef61
Foxrider <isachenko.foxrider@yandex.by>
2020-09-22 11:21:54 +0300

    join all parse list of dicts function in one "getDict()"
    # Conflicts:
    #	parse.py

--- parse.py
+++ parse.py
@@ -14,7 +14,7 @@ import config
 import requests
 from bs4 import BeautifulSoup
 
-#todo: изучить FOREIGN KEY
+# todo: изучить FOREIGN KEY
 
 conn = psycopg2.connect(
     host="localhost",
@@ -24,39 +24,34 @@ conn = psycopg2.connect(
 
 cur = conn.cursor(cursor_factory=DictCursor)
 
-
-
 cur.close()
 conn.close()
+
 
 def loop():
     d = getDict()
     fillRoutes(d)
     fillTT(d)
 
-# эта функия парсит список словарей как из main из которого все потом будут браться
-def getDict():
-    pass
 
 # эта функия заполняет таблицу routes
-def fillRoutes():
+def fillRoutes(d):
     pass
+
 
 # заполняет таблицу TT попутно заполняя таблицу stops
-def fillTT():
+def fillTT(d):
     pass
 
 
+# collective function to parse information of buses and their direction to list of dicts
+def getDict():
+    html = get_html(config.URL)
+    if html.status_code != 200:
+        print("something is not good with parsed page")
 
-
-def get_html(url):
-    r = requests.get(url, headers=config.HEADERS)
-    return r
-
-
-# parse content from http://ap1.brest.by/shelude/
-def get_content_main(html):
-    soup = BeautifulSoup(html, 'html.parser')
+    # parse content from http://ap1.brest.by/shelude/
+    soup = BeautifulSoup(html.text, 'html.parser')
     items = soup.find_all('div', class_="collapse fade")
     buses = []
     i = 0
@@ -72,12 +67,29 @@ def get_content_main(html):
         i += 1
     return buses
 
-# collective function to parse information of buses and their direction to list of dicts
-def parse_main():
-    html = get_html(config.URL)
-    if html.status_code != 200:
-        print("something is not good with parsed page")
-    dict = get_content_main(html.text)
-    return dict
 
-parse_main()
+def get_html(url):
+    r = requests.get(url, headers=config.HEADERS)
+    return r
+
+
+# # parse content from http://ap1.brest.by/shelude/
+# def get_content_main(html):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     items = soup.find_all('div', class_="collapse fade")
+#     buses = []
+#     i = 0
+#     for item in items:
+#         buses.append({
+#             'number': config.NUMBERS_OF_BUSES[i],
+#             'first': item.find_next("div", class_='first').get_text(strip=True),
+#             'first_link': 'http://ap1.brest.by' + item.find("a").get('href'),
+#             'last': item.find_next("div", class_='last').get_text(strip=True),
+#             'last_link': 'http://ap1.brest.by' + item.find_next("div", class_='last').find("a").get('href')
+#
+#         })
+#         i += 1
+#     return buses
+
+
+# print(getDict())
