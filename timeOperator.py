e953bab146c916d32a467ecbe98b38a641979406
Anton <as005409@g.bstu.by>
2020-10-02 23:49:01 +0300

    [fix] add possibility to send different time when the next day is weekend(was weekday) or weekday(was weekend)

--- timeOperator.py
+++ timeOperator.py
@@ -1,21 +1,33 @@
 from datetime import datetime
 import RefreshDB
 
+
 def getTime(weekdayTime, weekendTime):
-    timeTable = [weekendTime if datetime.today().weekday() in (5, 6) else weekdayTime][0]
+    timeTable = RefreshDB.cutStringToFirstNum([weekendTime if datetime.today().weekday() in (5, 6) else weekdayTime][0])
     if timeTable == '-':
-        return "Today is no given route today"
+        return "Сегодня такого маршрута нет"
 
     now = str(datetime.today().time().hour) + ':' + str(datetime.today().time().minute)
 
-    timeTable = RefreshDB.cutStringToFirstNum(timeTable)
     times = timeTable.split(" - ")
 
-    # организовываю цикличность
+    # организовываю цикличность проходя все возможные случаи
     if now>times[-1]:
-        return times[0]
+        if datetime.today().weekday() == 4:
+            if weekendTime =='-':
+                return "в пн " + times[0]
+            we = (RefreshDB.cutStringToFirstNum(weekendTime)).split(" - ")
+            return we[0]
+
+        elif datetime.today().weekday() == 5 and weekendTime.find("субб")>0:
+            return "в пн " + times[0]
+
+        elif datetime.today().weekday() == 6:
+            wd = (RefreshDB.cutStringToFirstNum(weekdayTime)).split(" - ")
+            return wd[0]
+        else:
+            return times[0]
 
     for t in times:
         if t>=now:
             return t
-    return "Today is no given route today"
