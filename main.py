d876b30a8bfbe3544e413d731718f9da226d039b
Anton <as005409@g.bstu.by>
2020-10-03 18:38:11 +0300

    [fix] изменил для лучшего чтения кода и изменения переменных в будущем

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def numberHandler(message):
         markup.add(itembtn1)
         markup.add(itembtn2)
 
-        bot.send_message(message.chat.id, "Выберите направление:", reply_markup = markup)
+        bot.send_message(message.chat.id, message.text + "\nВыберите направление:", reply_markup = markup)
 
         # updating table userdecision
         userTableWorker.setAll(message.chat.id, message.text, None, None, config.States.S_CHOOSE_DIR.value)
@@ -66,7 +66,7 @@ def callback_inline_Directions_Handler(call):
         for st in stops:
             markup.add(types.InlineKeyboardButton(text=st,callback_data=st))
 
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=dir,reply_markup=markup)
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=all[1]+"/"+dir,reply_markup=markup)
 
         # updating table userdecision
         userTableWorker.setAll(call.message.chat.id, all[1], dir, None, config.States.S_CHOOSE_BUS_STOP.value)
@@ -84,8 +84,17 @@ def callback_inline_Stops_Handler(call):
         weekendTime = userTableWorker.getTime(all[1],True,all[2],stop)[0]
         closestTime = timeOperator.getTime(weekdayTime,weekendTime)
 
-        Time = "Ближайшее время: " + closestTime +'\n' + weekdayTime+ '\n'+ weekendTime
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=Time,reply_markup=None)
+        if weekendTime == '-':
+            weekendTime = "в выходные дни: маршрутов нет"
+
+        history  = all[1] + "/" + all[2] + "/" + stop
+        clos_time = "Ближайшее время: " + closestTime
+        Time = weekdayTime+'\n'+weekendTime
+
+        # to mark these messages out
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=history,reply_markup=None)
+        bot.send_message(call.message.chat.id,text=clos_time)
+        bot.send_message(call.message.chat.id,text=Time)
 
         # reset table userdecision to begining
         userTableWorker.setAll(call.message.chat.id, None, None, None, config.States.S_ENTER_NUMBER.value)
