e39248bec81b90a407f7cb7478d3a84d4f3d2747
Anton <as005409@g.bstu.by>
2020-10-03 18:38:51 +0300

    [fix] add enum and change weekdays to enum version

--- timeOperator.py
+++ timeOperator.py
@@ -1,28 +1,45 @@
 from datetime import datetime
 import RefreshDB
+from enum import Enum
 
+class Weekdays(Enum):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
 
 def getTime(weekdayTime, weekendTime):
-    timeTable = RefreshDB.cutStringToFirstNum([weekendTime if datetime.today().weekday() in (5, 6) else weekdayTime][0])
+    timeTable = weekendTime if datetime.today().weekday() in (Weekdays.SATURDAY.value, Weekdays.SUNDAY.value) else weekdayTime
     if timeTable == '-':
         return "Сегодня такого маршрута нет"
+    # проверка на автобусы с раб + суб
+    if weekendTime.find("субб") > 0 and datetime.today().weekday() == Weekdays.SUNDAY.value:
+        return "Сегодня такого маршрута нет"
+
+    timeTable= RefreshDB.cutStringToFirstNum(timeTable)
 
     now = str(datetime.today().time().hour) + ':' + str(datetime.today().time().minute)
 
     times = timeTable.split(" - ")
 
+
+
+
     # организовываю цикличность проходя все возможные случаи
     if now>times[-1]:
-        if datetime.today().weekday() == 4:
+        if datetime.today().weekday() == Weekdays.FRIDAY.value:
             if weekendTime =='-':
                 return "в пн " + times[0]
             we = (RefreshDB.cutStringToFirstNum(weekendTime)).split(" - ")
             return we[0]
 
-        elif datetime.today().weekday() == 5 and weekendTime.find("субб")>0:
+        elif datetime.today().weekday() == Weekdays.SATURDAY.value and weekendTime.find("субб")>0:
             return "в пн " + times[0]
 
-        elif datetime.today().weekday() == 6:
+        elif datetime.today().weekday() == Weekdays.SUNDAY.value:
             wd = (RefreshDB.cutStringToFirstNum(weekdayTime)).split(" - ")
             return wd[0]
         else:
