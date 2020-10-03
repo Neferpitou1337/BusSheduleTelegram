b097f0c92ff4e8a13cc53ef36acde7ee288bc10c
Foxrider <isachenko.foxrider@yandex.by>
2020-10-03 19:47:56 +0300

    [fix] test

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def callback_inline_Stops_Handler(call):
         userTableWorker.setAll(call.message.chat.id, None, None, None, config.States.S_ENTER_NUMBER.value)
 
 
-# [later] collective function to initialize scheduler of loop() in RefreshDB that will update all tables once a day URL=https://overcoder.net/q/37667/%D0%BA%D0%B0%D0%BA%D0%BE%D0%B2-%D0%BD%D0%B0%D0%B8%D0%BB%D1%83%D1%87%D1%88%D0%B8%D0%B9-%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1-%D0%BF%D0%BE%D0%B2%D1%82%D0%BE%D1%80%D0%BD%D0%BE-%D0%B2%D1%8B%D0%BF%D0%BE%D0%BB%D0%BD%D1%8F%D1%82%D1%8C-%D1%84%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8E-%D0%BA%D0%B0%D0%B6%D0%B4%D1%8B%D0%B5-x-%D1%81%D0%B5%D0%BA%D1%83%D0%BD%D0%B4-%D0%B2-python
+# [later] collective function to initialize scheduler of loop() in RefreshDB that will update all tables once a day
 def RefreshDB_schedule(url):
     pass
 
