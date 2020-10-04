c5a78012d48a3a69b4d8797bc608e36feedd663c
Foxrider <isachenko.foxrider@yandex.by>
2020-10-04 17:52:32 +0300

    [fix] добавил имплеметацию isRefreshing() в main.py чтобы когда таблицы обновляются юзер не мог использовать бота

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import traceback
+
 import telebot
 from telebot import types
 from bs4 import BeautifulSoup
+
+import RefreshDB
 import config
 import requests
 import userTableWorker
 import hashlib
 import timeOperator
+import time
+import threading
 
 # initialization of bot
 bot = telebot.TeleBot(config.TOKEN, parse_mode=None)
@@ -25,6 +31,12 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: userTableWorker.getState(message.chat.id) == config.States.S_ENTER_NUMBER.value,
                      content_types=['text'])
 def numberHandler(message):
+    # проверка на обновление дб
+    if RefreshDB.isRefreshing():
+        bot.send_message(message.chat.id,text="Подождите пару минут, идет обновление базы данных")
+        return 0
+
+
     if message.text not in config.NUMBERS_OF_BUSES:
         bot.reply_to(message, "Попытайтесь написать русскими буквами или такого автобуса не существует или Создатель не знает о его появлении")
     else:
@@ -51,6 +63,11 @@ def numberHandler(message):
 @bot.callback_query_handler(
     func=lambda call: userTableWorker.getState(call.message.chat.id) == config.States.S_CHOOSE_DIR.value)
 def callback_inline_Directions_Handler(call):
+    # проверка на обновление дб
+    if RefreshDB.isRefreshing():
+        bot.send_message(call.message.chat.id,text="Подождите пару минут, идет обновление базы данных")
+        return 0
+
     if call.message:
         all = userTableWorker.getAll(call.message.chat.id)
 
@@ -72,10 +89,15 @@ def callback_inline_Directions_Handler(call):
         userTableWorker.setAll(call.message.chat.id, all[1], dir, None, config.States.S_CHOOSE_BUS_STOP.value)
 
 
-
+# giving a time to user and return to Entering number state
 @bot.callback_query_handler(
     func=lambda call: userTableWorker.getState(call.message.chat.id) == config.States.S_CHOOSE_BUS_STOP.value)
 def callback_inline_Stops_Handler(call):
+    # проверка на обновление дб
+    if RefreshDB.isRefreshing():
+        bot.send_message(call.message.chat.id,text="Подождите пару минут, идет обновление базы данных")
+        return 0
+
     if call.message:
         all = userTableWorker.getAll(call.message.chat.id)
         stop = call.data
@@ -101,12 +123,21 @@ def callback_inline_Stops_Handler(call):
 
 
 # [later] collective function to initialize scheduler of loop() in RefreshDB that will update all tables once a day
-def RefreshDB_schedule(url):
-    pass
+def RefreshDB_schedule(delay, task):
+    next_time = time.time() + delay
+    task()
+    while True:
+        time.sleep(max(0, next_time - time.time()))
+        try:
+            task()
+        except Exception:
+            traceback.print_exc()
+
+        # skip tasks if we are behind schedule:
+        next_time += (time.time() - next_time) // delay * delay + delay
 
 
 # RUN
-
-# print(config.States.S_ENTER_NUMBER, "\n", config.States.S_ENTER_NUMBER)
+threading.Thread(target=lambda: RefreshDB_schedule(24*60*60, RefreshDB.loop)).start()
 bot.polling(none_stop=True)
 
