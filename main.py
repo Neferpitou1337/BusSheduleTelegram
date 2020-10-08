bf52fea410032eb3b622fbff2fd676785086faa7
Foxrider <isachenko.foxrider@yandex.by>
2020-10-08 18:51:33 +0300

    [fix] добавил хандлер направлений а также конечный вывод времени

--- main.py
+++ main.py
@@ -130,11 +130,12 @@ def callback_inline_s2_Stop_Handler(call):
 
 
 @bot.callback_query_handler(func=lambda call: userTableWorker.getState(call.message.chat.id) == config.States.S2_ROUTE_HANDLER.value)
-def callback_inline_routes_handler(call):
+def callback_inline_s2_routes_handler(call):
     # проверка на обновление дб
     if RefreshDB.isRefreshing():
         bot.send_message(call.message.chat.id,text="Подождите пару минут, идет обновление базы данных")
         return 0
+
     if call.message:
         stop = userTableWorker.getAll(call.message.chat.id)[3]
         route = call.data
@@ -148,6 +149,39 @@ def callback_inline_routes_handler(call):
                                   text="Выберите направление:", reply_markup=markup)
 
         userTableWorker.setAll(call.message.chat.id, route, None, stop, config.States.S2_DIR_HANDLER.value)
+
+
+@bot.callback_query_handler(func=lambda call: userTableWorker.getState(call.message.chat.id) == config.States.S2_DIR_HANDLER.value)
+def callback_inline_s2_dir_handler(call):
+    # проверка на обновление дб
+    if RefreshDB.isRefreshing():
+        bot.send_message(call.message.chat.id,text="Подождите пару минут, идет обновление базы данных")
+        return 0
+
+    if call.message:
+        route = userTableWorker.getAll(call.message.chat.id)[1]
+        stop = userTableWorker.getAll(call.message.chat.id)[3]
+        dirs = userTableWorker.getS2Directions(route,stop)
+
+        # get choosen direction
+        for d in dirs:
+            if hashlib.md5(d.encode()).hexdigest() == call.data:
+                dir = d
+                break
+
+        weekdayTime = userTableWorker.getTime(route,False,dir, stop)[0]
+        weekendTime = userTableWorker.getTime(route,True,dir,stop)[0]
+        closestTime = timeOperator.getTime(weekdayTime,weekendTime)
+
+        clos_time = "Ближайшее время: " + closestTime
+        Time = weekdayTime+'\n'+weekendTime
+
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Время: ",reply_markup=None)
+        bot.send_message(call.message.chat.id,text=clos_time)
+        bot.send_message(call.message.chat.id,text=Time)
+
+        # reset table userdecision to begining
+        userTableWorker.setAll(call.message.chat.id, None, None, None, config.States.S_ENTER_NUMBER_OR_STOP.value)
 
 
 # handle direction button and give n Stops buttons
