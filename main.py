8cd7824c8ac80a8d7dfaa72c559e8417c46a228a
Foxrider <isachenko.foxrider@yandex.by>
2020-10-13 00:57:25 +0300

    [fix][doc] add some documentation and realization of favorites routes reply keyboard and configuration of it

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import RefreshDB
 import config
 import etc
+import favoritesdb
 
 import userTableWorker
 import hashlib
@@ -24,10 +25,13 @@ bot = telebot.TeleBot(config.TOKEN, parse_mode=None)
 
 
 
+"""
+    creation of flask site and webhook
+"""
+
 @app.route("/", methods=["HEAD","GET"])
 def index():
     return ''
-
 
 # steal from official github, setting the webhook to ngrock tunnel server
 @app.route('/', methods=['POST'])
@@ -41,15 +45,72 @@ def webhook():
         flask.abort(403)
 
 
+
+
+"""
+    Begining and reseting commands
+"""
+
 # handler of /start and /reset commands
 @bot.message_handler(commands=['start', 'reset'])
 def send_welcome(message):
     # instead of gitub.com it should open my github with this project
     bot.reply_to(message, "Добро Пожаловать в Bus Schedule Bot\nКак пользоваться:\nhttps://clck.ru/RMJMW",
-                 disable_web_page_preview=False)
-
+                 disable_web_page_preview=False,reply_markup=GetFavoritesMarkup(message))
     userTableWorker.setState(message.chat.id, etc.States.S_ENTER_NUMBER_OR_STOP.value)
 
+
+
+
+"""
+    Configure Favorites Reply Markup and Get Favorites Reply Markup
+"""
+
+def GetFavoritesMarkup(message):
+    favs = favoritesdb.getFavorites(message.chat.id)
+
+    markup = types.ReplyKeyboardMarkup(row_width=3,resize_keyboard=1,one_time_keyboard=True)
+    if favs == []:
+        markup.row(types.KeyboardButton("не сущ."), types.KeyboardButton("не сущ."),types.KeyboardButton("не сущ."))
+        markup.row(types.KeyboardButton("/config"))
+    else:
+        markup.row(types.KeyboardButton(favs[0]),types.KeyboardButton(favs[1]),types.KeyboardButton(favs[2]))
+        markup.row(types.KeyboardButton("/config"))
+    return markup
+
+
+@bot.message_handler(commands=['config'])
+def configure(message):
+    bot.send_message(message.chat.id, "Введите 3 своих любимых автобуса через пробел", reply_markup=types.ReplyKeyboardRemove(selective=False))
+    userTableWorker.setState(message.chat.id, etc.States.ENTER_FAV.value)
+
+@bot.message_handler(func=lambda message: userTableWorker.getState(message.chat.id) == etc.States.ENTER_FAV.value,
+                     content_types=['text'])
+def favorites(message):
+    count = 0
+    # запрос удаляя при этом старую reply клавиатуру
+    list_of_fav_buses = message.text.upper().split(' ')
+    print(list_of_fav_buses)
+    for bus in list_of_fav_buses:
+        if bus in etc.NUMBERS_OF_BUSES:
+            count+=1
+            print("count"+str(count))
+
+    if count >= 3:
+        favoritesdb.setFavorites(message.chat.id, *list_of_fav_buses)
+
+        bot.send_message(message.chat.id, "Конфигурация любимых автобусов закончена",
+                         reply_markup=GetFavoritesMarkup(message))
+        userTableWorker.setState(message.chat.id, etc.States.S_ENTER_NUMBER_OR_STOP.value)
+    else:
+        bot.send_message(message.chat.id, "Ошибка. Введите 3 своих любимых автобуса через пробел")
+
+
+
+
+"""
+    General program
+"""
 # get directions from table and make 2 buttons inside bot
 @bot.message_handler(func=lambda message: userTableWorker.getState(message.chat.id) == etc.States.S_ENTER_NUMBER_OR_STOP.value,
                      content_types=['text'])
@@ -190,10 +251,12 @@ def callback_inline_s2_dir_handler(call):
 
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=history,reply_markup=None)
         bot.send_message(call.message.chat.id,text=clos_time)
-        bot.send_message(call.message.chat.id,text=Time)
+        bot.send_message(call.message.chat.id,text=Time,reply_markup=GetFavoritesMarkup(call.message))
 
         # reset table userdecision to begining
         userTableWorker.setAll(call.message.chat.id, None, None, None, etc.States.S_ENTER_NUMBER_OR_STOP.value)
+
+
 
 
 # handle direction button and give n Stops buttons
@@ -253,7 +316,7 @@ def callback_inline_Stops_Handler(call):
         # to mark these messages out
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=history,reply_markup=None)
         bot.send_message(call.message.chat.id,text=clos_time)
-        bot.send_message(call.message.chat.id,text=Time)
+        bot.send_message(call.message.chat.id,text=Time, reply_markup=GetFavoritesMarkup(call.message))
 
         # reset table userdecision to begining
         userTableWorker.setAll(call.message.chat.id, None, None, None, etc.States.S_ENTER_NUMBER_OR_STOP.value)
