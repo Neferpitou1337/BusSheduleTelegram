8d2e1de890bab7f291c1dd5cc98d3bbf8e856e49
Foxrider <isachenko.foxrider@yandex.by>
2020-10-16 00:36:48 +0300

    [fix] end function back_handler that handle /back command

--- handlers/BackResetReplyButtons.py
+++ handlers/BackResetReplyButtons.py
@@ -3,6 +3,7 @@ from telebot import types
 import etc
 import userTableWorker
 from etc import bot
+from handlers import Fork_of_Stops_or_Numbers, From_Stop_handlers
 from handlers.All_relatedTo_Favorites import GetFavoritesMarkup
 
 
@@ -13,13 +14,22 @@ def GetBackResetMarkup():
 
 @bot.message_handler(commands=['back'])
 def back_handler(message):
-    # bot.edit_message_text("лол",message.chat.id, message.message_id-1)
-    pass
+    if userTableWorker.getState(message.chat.id) == etc.States.S_CHOOSE_BUS_STOP.value:
+        message.text = userTableWorker.getAll(message.chat.id)[1]
+        bot.delete_message(message.chat.id,message.message_id-1)
+        Fork_of_Stops_or_Numbers.numberHandler(message)
+        # [411279120, '10', 'Газоаппарат - БЭТЗ', None, '3']
+    elif userTableWorker.getState(message.chat.id) == etc.States.S2_DIR_HANDLER.value:
+        bot.delete_message(message.chat.id,message.message_id-1)
+        message.text = userTableWorker.getAll(message.chat.id)[3]
+        From_Stop_handlers.back_s2_Stop_Handler(message)
+        # [411279120, '3', None, 'Мицкевича', '6']
+    else:
+        reset_handler(message)
 
 
 @bot.message_handler(commands=['reset'])
 def reset_handler(message):
-    print(userTableWorker.getState(message.chat.id))
     if userTableWorker.getState(message.chat.id) == etc.States.S_ENTER_NUMBER_OR_STOP.value:
         return 0
     else:
