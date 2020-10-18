87fcea68f3e61fbacaf6fceb37a7e7ce909acbc0
Foxrider <isachenko.foxrider@yandex.by>
2020-10-18 16:15:53 +0300

    [fix] впилил блок try-catch в удаление сообщений
    на случай если кто-нибудь будет прожимать /back или /reset через 48 часов после отправки сообщения

--- handlers/BackResetReplyButtons.py
+++ handlers/BackResetReplyButtons.py
@@ -19,11 +19,14 @@ def back_handler(message):
             return 0
 
         # delete previous message that relate to timetable and naxt after before with "Ответ сервера"
-        bot.delete_message(message.chat.id,mess_id-1)
-        bot.delete_message(message.chat.id,mess_id)
-
-        message.text = userTableWorker.getAll(message.chat.id)[1]
-        Fork_of_Stops_or_Numbers.numberHandler(message)
+        try:
+            bot.delete_message(message.chat.id,mess_id-1)
+            bot.delete_message(message.chat.id,mess_id)
+        except:
+            pass
+        finally:
+            message.text = userTableWorker.getAll(message.chat.id)[1]
+            Fork_of_Stops_or_Numbers.numberHandler(message)
         # [411279120, '10', 'Газоаппарат - БЭТЗ', None, '3']
     elif userTableWorker.getState(message.chat.id) == etc.States.S2_DIR_HANDLER.value:
         if RefreshDB.isRefreshing():
@@ -31,11 +34,16 @@ def back_handler(message):
             return 0
 
         # delete previous message that relate to timetable and naxt after before with "Ответ сервера"
-        bot.delete_message(message.chat.id,mess_id-1)
-        bot.delete_message(message.chat.id,mess_id)
-
-        message.text = userTableWorker.getAll(message.chat.id)[3]
-        From_Stop_handlers.back_s2_Stop_Handler(message)
+        try:
+            bot.delete_message(message.chat.id,mess_id-1)
+            bot.delete_message(message.chat.id,mess_id)
+        except:
+            pass
+        finally:
+            message.text = userTableWorker.getAll(message.chat.id)[3]
+            From_Stop_handlers.back_s2_Stop_Handler(message)
+        # message.text = userTableWorker.getAll(message.chat.id)[3]
+        # From_Stop_handlers.back_s2_Stop_Handler(message)
         # [411279120, '3', None, 'Мицкевича', '6']
     else:
         reset_handler(message)
@@ -48,8 +56,11 @@ def reset_handler(message):
     else:
         mess_id = userTableWorker.getAll(message.chat.id)[-1]
         # delete previous message that relate to timetable and naxt after before with "Ответ сервера"
-        bot.delete_message(message.chat.id,mess_id-1)
-        bot.delete_message(message.chat.id,mess_id)
-
-        bot.send_message(message.chat.id,text="Введите номер автобуса или остановку",reply_markup=GetFavoritesMarkup(message))
-        userTableWorker.setAll(message.chat.id, None, None, None, etc.States.S_ENTER_NUMBER_OR_STOP.value, None)
+        try:
+            bot.delete_message(message.chat.id,mess_id-1)
+            bot.delete_message(message.chat.id,mess_id)
+        except:
+            pass
+        finally:
+            bot.send_message(message.chat.id,text="Введите номер автобуса или остановку",reply_markup=GetFavoritesMarkup(message))
+            userTableWorker.setAll(message.chat.id, None, None, None, etc.States.S_ENTER_NUMBER_OR_STOP.value, None)
